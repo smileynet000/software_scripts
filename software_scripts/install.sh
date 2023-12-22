715af07d22828ac5ccdcbc1958387a1397452f58
smileynet000 <67773697+smileynet000@users.noreply.github.com>
2023-12-21 21:59:49 -0800

    initial

---
+++ software_scripts/install.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+
+echo "Installing..."
+sudo cp -f scripts/* /usr/bin
+sudo chmod +x /usr/bin/addsoftware-client.sh
+echo
---
+++ software_scripts/scripts/addsoftware-client.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+echo Installing software...
+echo
+echo
+
+echo System packages...
+apt install git vlc audacity dia kdenlive kdeconnect nodejs npm
+apt install mint-backgrounds-*
+apt install virtualbox virtualbox-guest-additions-iso virtualbox-ext-pack virtualbox-guest-utils virtualbox-guest-x11
+apt install hardinfo
+apt install docker
+
+echo
+sleep 11
+
+echo Special system packages...
+sudo npm install -g tldr
+echo
+sleep 11
+
+echo flatpaks...
+sudo flatpak install org.gimp.GIMP com.obsproject.Studio org.inkscape.Inkscape us.zoom.Zoom
+sudo flatpak install …github.shiftey.Desktop io.github.slgobinath.SafeEyes org.linux_hardware.hw-probe
+echo
+sleep 11
+
+echo Done!
+echo
+sleep 11
